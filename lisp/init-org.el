3c3fdd8109d99e078d26ec595cdf20a5c9a252ef
Leo He <leodream2008@gmail.com>
2014-03-20 22:57:26 +0800

    Align tags in agenda views.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -19,7 +19,8 @@
       org-fast-tag-selection-single-key 'expert
       org-html-validation-link nil
       org-export-kill-product-buffer-when-displayed t
-      org-tags-column 80)
+      org-tags-column 80
+      org-agenda-tags-column -100)
 
 
 ;; Lots of stuff from http://doc.norang.ca/org-mode.html
