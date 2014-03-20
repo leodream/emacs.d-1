4583b3136ce3398bc639e6d68278a3370877df56
Leo He <leodream2008@gmail.com>
2014-03-20 22:38:41 +0800

    newsticker,select_current_word,kill_emacs_server
    Remove newsticker.
    Add function to select the word under cursor.
    Add function to kill emacs server using client.

--- init-leo.el
+++ init-leo.el
@@ -127,21 +127,6 @@
 
 
 ;;----------------------------------------------------------------------------
-;; newsticker and w3m
-;;----------------------------------------------------------------------------
-(require 'newsticker)
-(require 'w3m)
-
-(autoload 'w3m-region "w3m" nil t)
-(setq newsticker-html-renderer 'w3m-region)
-(setq w3m-default-display-inline-images t)
-
-(setq w3m-display-inline-images t)
-
-
-
-
-;;----------------------------------------------------------------------------
 ;; sdcv
 ;;----------------------------------------------------------------------------
 (load-file (expand-file-name "~/.emacs.d/sdcv.el"))
@@ -155,7 +140,139 @@
 (setq sdcv-dictionary-simple-list        ;; a simple dictionary list
       '(
         "朗道英汉字典5.0"
+        "朗道汉英字典5.0"
         ))
+
+
+
+
+;;----------------------------------------------------------------------------
+;; select current word. Twrice to select all woard in ""
+;;----------------------------------------------------------------------------
+;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
+(defun semnav-up (arg)
+  (interactive "p")
+  (when (nth 3 (syntax-ppss))
+    (if (> arg 0)
+        (progn
+          (skip-syntax-forward "^\"")
+          (goto-char (1+ (point)))
+          (decf arg))
+      (skip-syntax-backward "^\"")
+      (goto-char (1- (point)))
+      (incf arg)))
+  (up-list arg))
+
+;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
+(defun extend-selection (arg &optional incremental)
+  "Select the current word.
+Subsequent calls expands the selection to larger semantic unit."
+  (interactive (list (prefix-numeric-value current-prefix-arg)
+                     (or (region-active-p)
+                         (eq last-command this-command))))
+  (if incremental
+      (progn
+        (semnav-up (- arg))
+        (forward-sexp)
+        (mark-sexp -1))
+    (if (> arg 1)
+        (extend-selection (1- arg) t)
+      (if (looking-at "\\=\\(\\s_\\|\\sw\\)*\\_>")
+          (goto-char (match-end 0))
+        (unless (memq (char-before) '(?\) ?\"))
+          (forward-sexp)))
+      (mark-sexp -1))))
+
+(global-set-key (kbd "M-8") 'extend-selection)
+
+
+
+;;----------------------------------------------------------------------------
+;; Kill emacs server using client
+;;----------------------------------------------------------------------------
+(defun client-save-kill-emacs(&optional display)
+  " This is a function that can bu used to shutdown save buffers and
+shutdown the emacs daemon. It should be called using
+emacsclient -e '(client-save-kill-emacs)'.  This function will
+check to see if there are any modified buffers or active clients
+or frame.  If so an x window will be opened and the user will
+be prompted."
+
+  (let (new-frame modified-buffers active-clients-or-frames)
+
+    ; Check if there are modified buffers or active clients or frames.
+    (setq modified-buffers (modified-buffers-exist))
+    (setq active-clients-or-frames ( or (> (length server-clients) 1)
+                                        (> (length (frame-list)) 1)
+                                       ))
+
+    ; Create a new frame if prompts are needed.
+    (when (or modified-buffers active-clients-or-frames)
+      (when (not (eq window-system 'x))
+        (message "Initializing x windows system.")
+;        (x-initialize-window-system)
+        )
+      (when (not display) (setq display (getenv "DISPLAY")))
+      (message "Opening frame on display: %s" display)
+      (select-frame (make-frame-on-display display '((window-system . x)))))
+
+    ; Save the current frame.
+    (setq new-frame (selected-frame))
+
+
+    ; When displaying the number of clients and frames:
+    ; subtract 1 from the clients for this client.
+    ; subtract 2 from the frames this frame (that we just created) and the default frame.
+    (when ( or (not active-clients-or-frames)
+               (yes-or-no-p (format "There are currently %d clients and %d frames. Exit anyway?" (- (length server-clients) 1) (- (length (frame-list)) 2))))
+
+      ; If the user quits during the save dialog then don't exit emacs.
+      ; Still close the terminal though.
+      (let((inhibit-quit t))
+             ; Save buffers
+        (with-local-quit
+          (save-some-buffers))
+
+        (if quit-flag
+          (setq quit-flag nil)
+          ; Kill all remaining clients
+          (progn
+            (dolist (client server-clients)
+              (server-delete-client client))
+                 ; Exit emacs
+            (kill-emacs)))
+        ))
+
+    ; If we made a frame then kill it.
+    (when (or modified-buffers active-clients-or-frames) (delete-frame new-frame))
+    )
+  )
+
+
+(defun modified-buffers-exist()
+  "This function will check to see if there are any buffers
+that have been modified.  It will return true if there are
+and nil otherwise. Buffers that have buffer-offer-save set to
+nil are ignored."
+  (let (modified-found)
+    (dolist (buffer (buffer-list))
+      (when (and (buffer-live-p buffer)
+                 (buffer-modified-p buffer)
+                 (not (buffer-base-buffer buffer))
+                 (or
+                  (buffer-file-name buffer)
+                  (progn
+                    (set-buffer buffer)
+                    (and buffer-offer-save (> (buffer-size) 0))))
+                 )
+        (setq modified-found t)
+        )
+      )
+    modified-found
+    )
+  )
+
+
 
 
 
