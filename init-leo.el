9ab86fd18eecf67938003ff7be2d5501fae607ad
Leo He <leodream2008@gmail.com>
2014-05-25 14:30:16 +0800

    Delete wy-go-to-char as already have ace-jump-mode.

--- init-leo.el
+++ init-leo.el
@@ -396,29 +396,4 @@ nil are ignored."
         (t (self-insert-command (or arg 1)))))
 
 
-
-(defun wy-go-to-char (n char)
-  "Move forward to Nth occurence of CHAR.
-Typing `wy-go-to-char-key' again will move forwad to the next Nth
-occurence of CHAR."
-  (interactive "p\ncGo to char: ")
-  (search-forward (string char) nil nil n)
-  (while (char-equal (read-char)
-                     char)
-    (search-forward (string char) nil nil n))
-  (setq unread-command-events (list last-input-event)))
-(defun wy-go-to-char2 (n char)
-  "Move forward to Nth occurence of CHAR.
-Typing `wy-go-to-char-key' again will move forwad to the next Nth
-occurence of CHAR."
-  (interactive "p\ncGo to char: ")
-  (search-backward (string char) nil nil n)
-  (while (char-equal (read-char)
-                     char)
-    (search-backward (string char) nil nil n))
-  (setq unread-command-events (list last-input-event)))
-
-(define-key global-map (kbd "C-Z") 'wy-go-to-char2)
-
-
 (provide 'init-leo)
