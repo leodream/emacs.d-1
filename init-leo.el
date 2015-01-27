fed6cda668bfae3f4a70f1725b116104c55855c6
Leo He <leodream2008@gmail.com>
2015-01-27 11:38:28 +0800

    Add god-mode.

--- init-leo.el
+++ init-leo.el
@@ -13,6 +13,7 @@
 (require-package 'w3m)
 (require-package 'yasnippet)
 (require-package 'better-registers)
+(require-package 'god-mode)
 
 ;;----------------------------------------------------------------------------
 ;; Goto-the-last-change
@@ -430,6 +431,23 @@ nil are ignored."
 
 
 
+;;---------------------------------------------------------------------------
+;; god-mode
+;;---------------------------------------------------------------------------
+(require 'god-mode)
+(global-set-key (kbd "<escape>") 'god-mode-all)
+(setq god-exempt-major-modes nil)
+(setq god-exempt-predicates nil)
+(require 'god-mode-isearch)
+(define-key isearch-mode-map (kbd "<escape>") 'god-mode-isearch-activate)
+(define-key god-mode-isearch-map (kbd "<escape>") 'god-mode-isearch-disable)
+(define-key god-local-mode-map (kbd "z") 'repeat)
+(define-key god-local-mode-map (kbd "i") 'god-local-mode)
+(defun god-toggle-on-overwrite ()
+  "Toggle god-mode on overwrite-mode."
+  (if (bound-and-true-p overwrite-mode)
+      (god-local-mode-pause)
+    (god-local-mode-resume)))
 
 
 
