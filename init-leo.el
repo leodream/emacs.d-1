1e31d4365b4b6be57d02633d63ced2dc209742f9
Leo He <leodream2008@gmail.com>
2015-03-18 21:10:04 +0800

    Scheme setting.

--- init-leo.el
+++ init-leo.el
@@ -17,6 +17,7 @@
 (require-package 'helm)
 (require-package 'projectile)
 (require-package 'helm-projectile)
+(require-package 'autopair)
 
 ;;----------------------------------------------------------------------------
 ;; Goto-the-last-change
@@ -606,5 +607,53 @@ nil are ignored."
            :publishing-function org-publish-attachment)
           ("org" :components ("org-notes" "org-static"))))
 
+
+
+;;------------------------
+;; scheme setting
+;;------------------------
+(require 'cmuscheme)
+
+(defun kh/get-scheme-proc-create ()
+  "Create one scheme process if no one is created."
+  (unless (and scheme-buffer
+               (get-buffer scheme-buffer)
+               (comint-check-proc scheme-buffer))
+    (save-window-excursion
+      (run-scheme scheme-program-name))))
+
+(defun kh/scheme-send-last-sexp ()
+  "A replacement of original `scheme-send-last-sexp':
+1. check if scheme process exists, otherwise create one
+2. make sure the frame is splitted into two windows, current one is the scheme
+   source code window, the other one is the scheme process window
+3. run `scheme-send-last-sexp'
+
+PS: this function is inspired by Wang Yin."
+  (interactive)
+  (kh/get-scheme-proc-create)
+  (cond ((= 2 (count-windows))
+         (other-window 1)
+         (unless (string= (buffer-name)
+                          scheme-buffer)
+           (switch-to-buffer scheme-buffer))
+         (other-window 1))
+        (t
+         (delete-other-windows)
+         (split-window-vertically (floor (* 0.68 (window-height))))
+         (other-window 1)
+         (switch-to-buffer scheme-buffer)
+         (other-window 1)))
+  (scheme-send-last-sexp))
+
+(setq scheme-program-name "guile")
+
+(add-hook 'scheme-mode-hook
+          #'(lambda ()
+              (local-set-key (kbd "C-x C-M-e") 'kh/scheme-send-last-sexp)
+              (rainbow-delimiters-mode t)
+              (autopair-mode t)))
+
+(require 'autopair)
 
 (provide 'init-leo)
